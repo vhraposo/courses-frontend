c9cfba0bd0f8a2f3e43e2e86dfbf51d5b0dd31cc
vhraposo <thevhup@gmail.com>
2023-05-25 05:23:55 -0300

    add table component

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Course } from '../model/course';
 
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
   styleUrls: ['./courses.component.scss']
 })
-export class CoursesComponent {
+export class CoursesComponent implements OnInit {
 
+  courses: Course[] = [
+    {_id: '1', name: 'Angular', category: 'Front-End'}
+
+  ];
+  displayedColumns  = ['name', 'category'];
+
+  constructor(){
+  //  this.courses = [];
+  }
+
+    ngOnInit(): void {
+
+    }
 }
