559cd7daa105b86dd35e2d07ddb017d909e9db75
vhraposo <thevhup@gmail.com>
2023-05-25 23:43:58 -0300

    add observable and courses.json

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Course } from '../model/course';
 import { CoursesService } from '../services/courses.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-courses',
@@ -9,7 +10,7 @@ import { CoursesService } from '../services/courses.service';
 })
 export class CoursesComponent implements OnInit {
 
-  courses: Course[] = [];
+  courses: Observable<Course[]>;
   displayedColumns  = ['name', 'category'];
 
   //coursesService: CoursesService;
@@ -17,10 +18,11 @@ export class CoursesComponent implements OnInit {
   constructor(private coursesService: CoursesService){
   //  this.courses = [];
   //this.coursesService = new CoursesService();
+  this.courses = this.coursesService.list();
 }
 
 ngOnInit(): void {
-  this.courses = this.coursesService.list();
+
 
     }
 }
